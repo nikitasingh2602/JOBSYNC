198acf6d051189293ceb02989bd260f8b75b6567
nikitasingh2602 <120306375+nikitasingh2602@users.noreply.github.com>
2024-10-11 13:51:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#141414",
+        theme: "#6e00be",
+      },
+    },
+  },
+  plugins: [],
+};
